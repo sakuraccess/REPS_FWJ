9d5675561a6d9ff7273f3bd27314053dd810cfc1
Yinuo Zhao <sakuraccess@outlook.com>
2024-05-05 18:37:14 +0300

    1st ver

--- src/Main.scala
+++ src/Main.scala
@@ -1,7 +1,23 @@
 object Main {
+  import scala.io.StdIn.readLine
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
-    println("Hi hao.")
-    //wssb
+    println("Welcome to renewable energy plant system!")
+    var option: String = ""
+
+    while (option != "4") {
+      println("Option 1: generate the graph for dataset")
+      println("Option 2: analysis the data")
+      println("Option 3: Detect error")
+      option = scala.io.StdIn.readLine() // 从用户终端获取输入
+
+      if (option == "1") {
+        // 处理选项1的逻辑
+      } else if (option == "2") {
+        // 处理选项2的逻辑
+      } else if (option == "3") {
+        // 处理选项3的逻辑
+      }
+    }
   }
 }
