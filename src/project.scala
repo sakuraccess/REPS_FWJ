8e4633411c1f5b94ba861796ca891b660506935b
Alex <0415720602alex@gmail.com>
2024-05-05 17:19:39 +0300

    comment

---
+++ src/project.scala
@@ -0,0 +1,21 @@
+//object project extends App {
+//  import scala.io.StdIn
+//  println("Welcome to renewable energy plant system!")
+//  private var option : String
+//  while(option != "4"){
+//    println("Option 1: generate the graph for dataset")
+//    println("Option 2: analysis the data")
+//      println("Option 3: Detect error")
+//    option = StdIn.readLine()
+//    if (option == "1"){
+//
+//    }
+//    else if (option == "2"){
+//
+//    }
+//    else if (option == "3"){
+//
+//  }
+//  }
+//  println("Thank you for using! See you next time!")
+//}
